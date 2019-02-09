870df63ea507fc2784340bcddd10246d9b98ee54
Bespalov-AV <36464371+Bespalov-AV@users.noreply.github.com>
2019-02-09 15:40:14 +0700

    Вывод таблицы и работа со сторедж

--- PaymentClass.js
+++ PaymentClass.js
@@ -1,18 +1,36 @@
 const PaymentClass = function () {
   this.pymentString = {
+    id: 0,
     date: new Date(),
     summa: 0,
     client: '',
     comment: ''
   };
 
-  this.pymentData = [];
+  this.Data = [];
 
   this.setStorage = function() {
-      chrome.storage.local.set("pymentData", this.pymentData);
+      chrome.storage.local.set({"pymentData": this.Data});
   };
 
-    this.getStorage = function () {
-        this.pymentData = chrome.storage.local.get("pymentData");
-    };
+  this.getStorage = function () {
+      this.pymentData = chrome.storage.local.get(["pymentData"],  function(result) {
+        console.log('Value currently is ' + result);
+      });
+  };
+
+  this.addNewRow = (table, Data) => {
+    Data.forEach(element => {
+      const row = table.insertRow();
+      row.insertCell(0).textContent = element.id;
+      row.insertCell(1).textContent = element.date;
+      row.insertCell(2).textContent = element.summa;
+      row.insertCell(3).textContent = element.client;
+      row.insertCell(4).textContent = element.comment;
+    });
+
+  const addNewRow1 = () => {
+    return 1;
+  }
+  }
 };
--- popup.js
+++ popup.js
@@ -1,5 +1,30 @@
-
-
+console.log('Start1');
 document.addEventListener("DOMContentLoaded", function () {
    console.log('Start');
 });
+
+const Payment = new PaymentClass();
+
+//testdate
+const testData = {
+  id: 2,
+  date: new Date().toDateString(),
+  summa: 2500,
+  client: 'Федор',
+  comment: 'отчет'
+};
+Payment.Data.push(testData);
+//testdate
+
+const table = document.getElementById('payment-table');
+Payment.addNewRow(table, [testData]);
+
+Payment.setStorage();
+const res = chrome.storage.local.set({"data":55});
+console.log(res);
+
+const res2 = chrome.storage.local.get([{'data' : {}}], function(result) {return result});
+console.log(res2);
+
+const storegeData = Payment.getStorage();
+console.log(storegeData);
